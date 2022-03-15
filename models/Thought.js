564626813db20dfdd9493c2eec5658cfb0d2a664
Jude Collins <jude.collins13@yahoo.com>
2022-03-15 18:58:59 -0400

    refactoring thought js

--- models/Thought.js
+++ models/Thought.js
@@ -1,12 +1,43 @@
-const { Schema, model } = require('mongoose');
+
+// Require Mongoose and Moment
+const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
-const ThoughtSchema = new Schema({
-//const ThoughtSchema = new Schema(
+
+const ReactionsSchema = new Schema(
+    {
+    // Set ID 
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+    },
+    {
+    toJSON: {
+        getters: true
+    } 
+    }
+);
+
+// Thoughts Schema
+const ThoughtsSchema = new Schema(
     {
     thoughtText: {
         type: String,
-        required: true
         required: true,
         minlength: 1,
         maxlength: 280
@@ -14,15 +45,14 @@ const ThoughtSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now,
+     
         get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
     },
     username: {
-
         type: String,
-        required: trues
+        required: true
     },
-    reactions: {
-
+    // Use Reactions Schema 
     reactions: [ReactionsSchema]
     },
     {
@@ -32,9 +62,15 @@ const ThoughtSchema = new Schema({
     },
     id: false
     }
-//}) 
 )
 
-const Thought = model('Thought', ThoughtSchema);
+// get count of reactions
+ThoughtsSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
 
-module.exports = Thought; 
+
+const Thoughts = model('Thoughts', ThoughtsSchema);
+
+// Export Module
+module.exports = Thoughts;
