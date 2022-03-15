c0c3383e8d301cbd84ab726586b39cc05f6ce7aa
Jude Collins <jude.collins13@yahoo.com>
2022-03-15 19:19:35 -0400

    refactor user.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
+// Require Mongoose
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema({
+const UsersSchema = new Schema(
+    {
     username: {
         type: String,
         unique: true,
@@ -11,23 +13,34 @@ const UserSchema = new Schema({
         type: String,
         required: true,
         unique: true,
-        // REGEX to validate email
-        match: [/.+@.+\..+/]
+        // use REGEX to validate correct email
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
     },
     thoughts: [{
         type: Schema.Types.ObjectId,
-        ref: 'Thought'
+        ref: 'Thoughts'
     }],
     friends: [{
         type: Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'Users'
     }]
-},
-{
+    },
+    {
     toJSON: {
-        virtuals: true
+        virtuals: true,
+        getters: true,
     },
     id: false
+    }
+)
+
+// get count of friends
+UsersSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
 })
 
-module.exports = User; 
+// create the Users model 
+const Users = model('Users', UsersSchema);
+
+
+module.exports = Users;
