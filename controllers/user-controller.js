8c5bd1da17f6a363d1ec10bb95f5afd341ffebfb
Jude Collins <jude.collins13@yahoo.com>
2022-03-19 12:47:22 -0400

    user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -10,3 +10,39 @@ const usersController = {
         .then(dbUsersData => res.json(dbUsersData))
         .catch(err => res.status(400).json(err));
     },
+
+// Get All Users
+getAllUsers(req, res) {
+    Users.find({})
+    
+    .populate({path: 'thoughts', select: '-__v'})
+  
+    .populate({path: 'friends', select: '-__v'})
+    .select('-__v')
+  
+    .then(dbUsersData => res.json(dbUsersData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+},
+
+// Get single user 
+getUsersById({params}, res) {
+    Users.findOne({_id: params.id })
+    .populate({path: 'thoughts', select: '-__v'})
+    .populate({path: 'friends', select: '-__v'})
+    .select('-__v')
+    // return if no user found 
+    .then(dbUsersData => {
+        if(!dbUsersData) {
+            res.status(404).json({message: 'No User with this particular ID!'});
+            return; 
+        }
+        res.json(dbUsersData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err)
+    })
+},
