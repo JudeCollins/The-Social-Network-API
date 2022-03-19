691ddc41ff2ebf8e8dd556d028297539e6d6c4e5
Jude Collins <jude.collins13@yahoo.com>
2022-03-19 12:39:50 -0400

    thoughts-controller

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -19,3 +19,33 @@ const thoughtsController = {
         })
         .catch(err => res.json(err)); 
     },
+    // Get all  Thoughts
+    getAllThoughts(req,res) {
+        Thoughts.find({})
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-__v')
+        // .sort({_id: -1})
+        .then(dbThoughtsData => res.json(dbThoughtsData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    // Get certain thought 
+    getThoughtsById({params}, res) {
+        Thoughts.findOne({ _id: params.id })
+        .populate({path: 'reactions',select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtsData => {
+            if(!dbThoughtsData) {
+            res.status(404).json({message: 'No thoughts with this particular ID!'});
+            return;
+        }
+        res.json(dbThoughtsData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
