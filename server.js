3579b51eb0783a8d030f7d2e2208852aa11ee707
Jude Collins <jude.collins13@yahoo.com>
2022-03-30 20:26:12 -0400

    hopefully finished

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
-// Require express and mongoose
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./config/connection');
+const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
 
-app.use(express.static('public'));
-
-app.use(require('./routes'));
-
-// Connect mongoose
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/the-social-network-api', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
-
-// Logging mongoose queries
-mongoose.set('debug', true);
-
-app.listen(PORT, () => console.log(` ************** Connected on localhost:${PORT} ************* `));
